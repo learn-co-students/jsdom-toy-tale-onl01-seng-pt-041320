f5ca17db5c9c7788a464c45a1f27b77057021f28
vedransehovic <vedran.sehovic@gmail.com>
2020-11-02 07:25:36 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 let addToy = false;
+let divCollect = document.querySelector('#toy-collection')
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".add-toy-form");
+  let divCollect = document.querySelector('#toy-collection')
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -28,36 +30,76 @@ document.addEventListener("DOMContentLoaded", () => {
 function fetchToys() {
   return fetch("http://localhost:3000/toys")
   .then(resp => resp.json())
-  .then(json => renderToys(json));
+  .then(toys => {
+    toys.forEach(toy => {
+      //function to render toys goes here or something
+      renderToys(toy)
+    })
+  })
 }
 
-function renderToys(data) {
-  console.log(data)
-  const toyCollection = document.getElementById("toy-collection")
-  for(i=0; i<data.length; i++){
-    //call a function that will generate cards and append them to DOM element
-    toyCollection.append(generateCards(data[i]))
-  }
-}
+// getToys().then(toys => {
+//   toys.forEach(toy => {
+//     //function to render toys goes here or something
+//     renderToys(toy)
+//   })
+// })
+
+
+// function renderToys(data) {
+//   const toyCollection = document.getElementById("toy-collection")
+//   for(i=0; i<data.length; i++){
+//     //call a function that will generate cards and append them to DOM element
+//     toyCollection.append(generateCards(data[i]))
+//   }
+// }
+// function generateCards(cardInfo) {
+//   const card = document.createElement("div")
+//   card.setAttribute("class", "card")
+//   //generate toy cards HTML
+//     card.innerHTML = `<h2>${cardInfo.name}</h2>
+//     <img src=${cardInfo.image} class="toy-avatar" />
+//     <p data-number="${cardInfo.id}">${cardInfo.likes} </p>
+//     <button class="like-btn" data-id="${cardInfo.id}">Like <3</button>`
+//     const likeButton = document.querySelector(`[data-id="${cardInfo.id}"]`)
+    
+//    // console.log(likeButton)
+//     card.addEventListener('click', (event) => {
+//      //toyLikeDOM(cardInfo)
+     
+//      let likes = parseInt(event.target.previousElementSibling.innerText)
+//      likes++
+//      console.log(likes)
+//     } )
+
+//   return card
+// }
 
 
 //this function generates HTML for each card
-function generateCards(cardInfo) {
-  const card = document.createElement("div")
-  card.setAttribute("class", "card")
-  //generate toy cards HTML
-    card.innerHTML = `<h2>${cardInfo.name}</h2>
-    <img src=${cardInfo.image} class="toy-avatar" />
-    <p data-number="${cardInfo.id}">${cardInfo.likes} </p>
-    <button class="like-btn" data-id="${cardInfo.id}">Like <3</button>`
-    const likeButton = document.querySelector(`[data-id="${cardInfo.id}"]`)
-    console.log(likeButton)
-    likeButton.addEventListener('click', (e) => {
-      console.log(e.target.dataset);
-      likes(e)
-    })
+function renderToys(toy) {
+  let h2 = document.createElement('h2')
+  h2.innerText = toy.name
 
-  return card
+  let img = document.createElement('img')
+  img.setAttribute('src', toy.image)
+  img.setAttribute('class', 'toy-avatar')
+
+  let p = document.createElement('p')
+  p.innerText = `${toy.likes} likes`
+
+  let btn = document.createElement('button')
+  btn.setAttribute('class', 'like-btn')
+  btn.setAttribute('id', toy.id)
+  btn.innerText = "like"
+  btn.addEventListener('click', (e) => {
+    likes(e)
+  })
+
+  let divCard = document.createElement('div')
+  divCard.setAttribute('class', 'card')
+  divCard.append(h2, img, p, btn)
+  divCollect.append(divCard)
 }
 
 //ADDING A NEW TOY HERE
@@ -92,23 +134,16 @@ function submitData(toyName, imgURL) {
 
 //LIKING A TOY
 
-function likes(e) {
-  e.preventDefault()
+function likes(e){
   let more = parseInt(e.target.previousElementSibling.innerText) + 1
+  e.target.previousElementSibling.innerText = `${more} likes`;
+}
 
-  fetch(`http://localhost:3000/toys/${e.target.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
+function toyLikeDOM(objectVar) {
+  let incrementedLikes = objectVar.likes + 1
+  pVar.innerText = incrementedLikes
+}
 
-      },
-      body: JSON.stringify({
-        "likes": more
-      })
-    })
-    .then(res => res.json())
-    .then((like_obj => {
-      e.target.previousElementSibling.innerText = `${more} likes`;
-    }))
+function toyLikeServerUpdate() {
+
 }
